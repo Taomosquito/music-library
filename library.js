a599b1eef8e1a340b6d5224220a36bd48ada5c60
Taomosquito <S_Peloquin123@live.com>
2024-05-27 14:33:52 -0400

    change all applicable references to library to this

--- library.js
+++ library.js
@@ -62,23 +62,23 @@ const library = {
   // t01: Code Monkey by Jonathan Coulton (Thing a Week Three)
   // t02: Model View Controller by James Dempsey (WWDC 2003)
   printPlaylist: function(playlistId) {
-    const playlist = library.playlists[playlistId];
+    const playlist = this.playlists[playlistId];
     const tracks = playlist["tracks"];
     const playlistName = playlist["name"];
     const tracksLength = tracks["length"];
     console.log(`${playlistId}: ${playlistName} - ${tracksLength} track(s)`);
     for (let i = 0; i < tracksLength; i++) {
       const trackItem = tracks[i];
-      const objTrackItemName = library["tracks"][trackItem]["name"];
-      const objTrackItemArtist = library["tracks"][trackItem]["artist"];
-      const objTrackItemAlbum = library["tracks"][trackItem]["album"];
+      const objTrackItemName = this["tracks"][trackItem]["name"];
+      const objTrackItemArtist = this["tracks"][trackItem]["artist"];
+      const objTrackItemAlbum = this["tracks"][trackItem]["album"];
       console.log(`${trackItem}: ${objTrackItemName} by ${objTrackItemArtist} (${objTrackItemAlbum})`);
     }
   },
   // adds an existing track to an existing playlist
   addTrackToPlaylist: function(trackId, playlistId) {
-    const track = library["tracks"][trackId]["id"];
-    const playlist = library["playlists"][playlistId];
+    const track = this["tracks"][trackId]["id"];
+    const playlist = this["playlists"][playlistId];
     const playlistTracks = playlist["tracks"];
     playlistTracks.push(track);
   },
@@ -89,8 +89,8 @@ const library = {
   },
   // adds a track to the library
   addTrack: function(name, artist, album) {
-    const fastTracks = library["tracks"];
-    newId = library.generateUid();
+    const fastTracks = this["tracks"];
+    newId = this.generateUid();
     fastTracks[newId] = {
       id: newId,
       name: name,
@@ -100,8 +100,8 @@ const library = {
   },
   // adds a playlist to the library
   addPlaylist: function(name) {
-    fastplaylists = library["playlists"];
-    newId = library.generateUid();
+    fastplaylists = this["playlists"];
+    newId = this.generateUid();
     fastplaylists[newId] = {
       id: newId,
       name: name,
